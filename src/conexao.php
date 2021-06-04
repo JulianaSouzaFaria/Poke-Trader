49e9679fee622b672b3892d17905760a9994ea25
Juliana Souza de Faria <78223755+JulianaSouzaFaria@users.noreply.github.com>
2021-06-04 15:09:43 -0300

    Add files via upload

--- src/conexao.php
+++ src/conexao.php
@@ -1,58 +1,15 @@
 <?php
-//pagina criada para conectar com banco de dados
 
-
-
-
-/*
-class Conexao {
-
-	private $host = 'localhost';
-	private $dbname = 'bxblue_poke';
-	private $user = 'root';
-	private $pass = '';
-
-	public function conectar(){
-		try{ //dados conexao
-			$conexao = new PDO(
-				"mysql:host=$this->host;dbname=$this->dbname", //local
-				"$this->user", //usuario
-				"$this->pass" //senha
-			);
-
-			return $conexao;
-		} catch (PDOException $e){ //msg caso tenha erro
-			echo'<p>'.$e->getMessage().'</p>';
-		}
-	}
-}
-
-*/
-
-
-
-/*
-$db = parse_url(getenv("DATABASE_URL"));
-$db["path"] = ltrim($db["path"], "");
-
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"], "ec2-18-214-140-149.compute-1.amazonaws.com",
-    $db["port"], "5432",
-    $db["user"], "ewsbgjzwkuylcr",
-    $db["pass"], "8700f4ffd184ca777de83a2cde24ce9111f8c784d23c39dc76adcbefce2fcd44",
-    ltrim($db["path"], "")
-));
-*/
 class Conexao {
 	public function conectar(){
 		try {
 			$dsn = "pgsql:host=ec2-18-214-140-149.compute-1.amazonaws.com;port=5432;dbname=d7ds5oehb3onqq;";
 			// make a database connection
-			$pdo = new PDO($dsn, "ewsbgjzwkuylcr", "8700f4ffd184ca777de83a2cde24ce9111f8c784d23c39dc76adcbefce2fcd44", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+			$conexao = new PDO($dsn, "ewsbgjzwkuylcr", "8700f4ffd184ca777de83a2cde24ce9111f8c784d23c39dc76adcbefce2fcd44", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-			if ($pdo) {
-				echo "Connected to the db database successfully!";
+			if ($conexao) {
+				//echo "Connected to the db database successfully!";
+				return $conexao;
 			}
 		} catch (PDOException $e) {
 			die($e->getMessage());
--- src/troca.service.php
+++ src/troca.service.php
@@ -6,7 +6,7 @@ class TrocaService{
 	private $troca;
 	private $tabela;
 
-	public function __construct(Conexao $conexao, Troca $troca, $tabela = "troca_pokemon") 
+	public function __construct(Conexao $conexao, Troca $troca, $tabela = "public.troca_pokemon") 
 	{
 		$this->conexao = $conexao->conectar();
 		$this->troca = $troca;
@@ -15,7 +15,7 @@ class TrocaService{
 
 	public function inserir()
 	{
-		$query = 'insert into '.$this->tabela.'(nome_pokemon,base_experience)values(:nome_pokemon,:base_experience)';
+		$query = 'insert into '.$this->tabela.'(nome_pokemon,base_experience,data_cadastro)values(:nome_pokemon,:base_experience,NOW())';
 		$stmt = $this->conexao->prepare($query);
 		$stmt->bindValue(':base_experience', $this->troca->__get('base_experience'));
 		$stmt->bindValue(':nome_pokemon', $this->troca->__get('nome_pokemon'));
@@ -29,7 +29,5 @@ class TrocaService{
 		$stmt->execute();
 		return $stmt->fetchAll(PDO::FETCH_OBJ);
 	}
-
 }
-
 ?>
